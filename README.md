3308b274ef66ebe62171783d7f35f2f3b6591f9c
ashishkaparwan17 <ashishsinghkaparwanask@gmail.com>
2022-12-14 05:07:08 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mock-exam-system-mean-backend
+
+This backend is developed using NodeJS, ExpressJS and MongoDB.
+
+Backend is hosted on render: https://mock-exam-system-mean-backend.onrender.com
+
+Frontend is hosted on firebase: https://exam-system-mean.web.app/
+
+Frontend repository: https://github.com/ashishkaparwan17/mock-exam-system-mean-frontend
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ server.use(routes)
 
 // change PROD_DB_NAME to DEV_DB_NAME if in dev mode
 mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jebdmx5.mongodb.net/${process.env.PROD_DB_NAME}?retryWrites=true&w=majority`
-).then(() => {
+    `mongodb://127.0.0.1:27017/${process.env.PROD_DB_NAME}`
+).then(()=>{
     server.listen(process.env.PORT || 3000)
     console.log("Success")
 }).catch((err) => {
