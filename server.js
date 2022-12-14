b18329a4bc15cb92302edceef174ea1ede4d6e16
ashishkaparwan17 <ashishsinghkaparwanask@gmail.com>
2022-12-14 21:27:08 +0530

    changed home ip

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ server.use(routes)
 
 // change PROD_DB_NAME to DEV_DB_NAME if in dev mode
 // connection using mongodb atlas
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jebdmx5.mongodb.net/${process.env.PROD_DB_NAME}?retryWrites=true&w=majority`
-).then(() => {
-    server.listen(process.env.PORT || 3000)
-    console.log("Success")
-}).catch((err) => {
-    console.log("Unable to connect to server: " + err)
-})
-
-// change PROD_DB_NAME to DEV_DB_NAME if in dev mode
-// local db connection
 // mongoose.connect(
-//     `mongodb://127.0.0.1:27017/${process.env.DEV_DB_NAME}`
-// ).then(()=>{
+//     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jebdmx5.mongodb.net/${process.env.PROD_DB_NAME}?retryWrites=true&w=majority`
+// ).then(() => {
 //     server.listen(process.env.PORT || 3000)
 //     console.log("Success")
 // }).catch((err) => {
 //     console.log("Unable to connect to server: " + err)
 // })
+
+// change PROD_DB_NAME to DEV_DB_NAME if in dev mode
+// local db connection
+mongoose.connect(
+    `mongodb://0.0.0.0:27017/${process.env.DEV_DB_NAME}`
+).then(()=>{
+    server.listen(process.env.PORT || 3000)
+    console.log("Success")
+}).catch((err) => {
+    console.log("Unable to connect to server: " + err)
+})
